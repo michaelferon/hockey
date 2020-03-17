6e7b9ee11f918483c40225211c0b4bd8c67793f3
Michael Feron <feron.michael@gmail.com>
2020-03-16 23:25:54 -0600

    More cleaning. Added description of data features.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 An analysis of NHL player data using modern statistical learning techniques.
+
+Data
+====
+Ten seasons of data were collected from the NHL's publicly available database at [__nhl.com/stats__](http://www.nhl.com/stats/) for the 2009-2010 season through the 2018-2019 season. Each observation corresponds to a set of statistics measured for a single hockey player in a single season. All in all, there are 8,853 observations over more than 100 variables.
+
+See `data/features.pdf` for a description of the features recorded.
+
+See `src/init.R` for the initial data cleaning.
--- src/init.R
+++ src/init.R
@@ -2,11 +2,13 @@ rm( list = ls() )
 
 # Make sure you're in the 'src' directory.
 
-library(dplyr)
-library(readr)
-library(tibble)
+library(dplyr)     # For data manipulation.
+library(readr)     # For read_csv().
+library(stringr)   # For str_sub().
+library(lubridate) # For handling Time objects.
 
 
+# Data files.
 files <- c('../data/clean/summary.csv',
            '../data/clean/faceoff.csv',
            '../data/clean/goals.csv',
@@ -16,9 +18,11 @@ files <- c('../data/clean/summary.csv',
            '../data/clean/pp.csv')
 
 
-df <- read_csv(files[1], na = c('--', '', 'NA'),
+
+# Read in data to df.
+df <- read_csv(files[1], na = c('--', '', 'NA'), # -- marks NA in these files.
                col_types = cols('TOI/GP' = col_character())) %>%
-      arrange(Player, Season, Team)
+      arrange(Player, Season, Team) # `TOI/GP` needs to be char.
 for (file in files[-1]) {
   temp <- read_csv(file, na = c('--', '', 'NA'),
                    col_types = cols('TOI/GP' = col_character())) %>%
@@ -29,9 +33,44 @@ for (file in files[-1]) {
 rm(file, files, temp)
 
 
+
+# Read in biographical data.
 bio <- read_csv('../data/clean/bio.csv', na = c('--', '', 'NA')) %>%
        arrange(Player, Team) %>%
-       .[, !(colnames(.) %in% colnames(df)) |
-             colnames(.) %in% c('Player', 'Team', 'S/C', 'Pos')]
+       .[, !(colnames(.) %in% colnames(df)) | # Removing some overlap.
+             colnames(.) %in% c('Player')]
 
-df <- df %>% inner_join(bio, by = c('Player', 'Team', 'S/C', 'Pos'))
+# There are 10 hockey players who share a set of 5 names. Tough to handle.
+doubleNames <- (bio %>% group_by(Player) %>% count %>% filter(n > 1))$Player
+df <- df %>% filter(!(Player %in% doubleNames))
+df <- df %>% left_join(bio, by = c('Player'))
+rm(doubleNames, bio)
+
+
+
+# A bunch of necessary manipulation.
+df <- df %>%
+  mutate(Season = as.character(Season), # Change season to first year, as char.
+         Season = str_sub(Season, 1, 4)) %>%
+  mutate(`1st Season` = as.character(`1st Season`), # Same thing here.
+         `1st Season` = str_sub(`1st Season`, 1, 4)) %>%
+  mutate(`Draft Yr` = as.character(`Draft Yr`)) %>% # Change to char.
+  mutate(`TOI/GP` = sapply(strsplit(df$`TOI/GP`, ':'), # char -> seconds.
+                           function(y) sum(as.numeric(y) * c(60, 1)))) %>%
+  # All these below convert Time to seconds.
+  mutate(`PP TOI/GP` = 60*hour(`PP TOI/GP`) + minute(`PP TOI/GP`)) %>%
+  mutate(`SH TOI/GP` = 60*hour(`SH TOI/GP`) + minute(`SH TOI/GP`)) %>%
+  mutate(`EV TOI/GP` = 60*hour(`EV TOI/GP`) + minute(`EV TOI/GP`)) %>%
+  mutate(`PIM/GP` = 60*hour(`PIM/GP`) + minute(`PIM/GP`))
+
+
+
+## TEST
+df <- df %>%
+  # High NA features.
+  select(-c('Draft Yr', 'Round', 'Overall', 'S/P', 'FOW%')) %>%
+  filter(GP >= 20) # Removing players with low GP.
+
+test <- apply(df, 2, function(x) mean(is.na(x)))
+test <- test[test > 0] # Proportion of NAs in certain features.
+
