37c11bbbf3ea3c7a941cb436b54ae09c3d0aee18
Michael Feron <feron.michael@gmail.com>
2020-03-18 12:32:08 -0600

    Added simple linear model (lots of singularities).

--- src/reg.R
+++ src/reg.R
@@ -9,10 +9,14 @@ library(lubridate)
 
 # Load `df` and `ds`.
 load('../data/data.Rdata')
-
-## INFO
 # df -- this is the complete dataset, unstandardized.
 # ds -- same as df, but standardized.
+
+
+
+# X is all the quantitative data.
+X <- df %>% select(-c(1:11))
+model <- lm(P ~ ., data = X)
 
 
 
