92a162cfcffcc115f3580c7a32f3f4180bb54222
Michael Feron <feron.michael@gmail.com>
2020-03-17 18:40:34 -0600

    Added files for regression and classification analysis.

---
+++ src/class.R
@@ -0,0 +1,19 @@
+rm( list = ls() )
+
+# Make sure you're in the 'src' directory.
+# CLASSIFICATION ANALYSIS
+
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+
+# Load `df` and `ds`.
+load('../data/data.Rdata')
+
+## INFO
+# df -- this is the complete dataset, unstandardized.
+# ds -- same as df, but standardized.
+
+
+
+
--- src/setup.R
+++ src/setup.R
@@ -3,8 +3,6 @@ rm( list = ls() )
 # Make sure you're in the 'src' directory.
 
 library(dplyr)
-library(ggplot2)
-library(lubridate)
 
 # Load `df` and `X` data objects.
 load('../data/dataFull.Rdata')
