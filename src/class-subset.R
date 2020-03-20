3e4f8ead58ad26c84eb58bac582f85b750cafde7
Michael Feron <feron.michael@gmail.com>
2020-03-20 00:14:50 -0600

    Implemented forward-selection for optimizing position LDA.

---
+++ src/class-subset.R
@@ -0,0 +1,104 @@
+rm( list = ls() )
+
+# Make sure you're in the 'src' directory.
+# CLASSIFICATION ANALYSIS
+
+library(ggplot2)
+library(lubridate)
+library(MASS)
+library(dplyr)
+library(CombMSC)
+
+# Load `df` and `ds`.
+load('../data/data.Rdata')
+load('../data/bio.Rdata')
+
+## INFO
+# df -- this is the complete dataset, unstandardized.
+# ds -- same as df, but standardized.
+
+
+dep <- c('P', 'SHG', 'SHP', 'FO', 'EV FO', 'PP FO', 'SH FO', 'SH FOW', 'SH FOL',
+         'OZ FO', 'NZ FO', 'DZ FO', 'On-Ice EV GD', 'ENP', 'Net Pen',
+         'Net Pen/60', 'G Msct', 'SHA', 'SHA2', 'PPA', 'PPA2')
+
+dc <- df %>%
+  .[, !(names(df) %in% dep)] %>%
+  dplyr::select(-c(1:2, 5:7, 9:11)) %>%
+  as.data.frame
+dc$Pos <- as.factor(dc$Pos)
+dc$Ntnlty <- as.factor(dc$Ntnlty)
+dc$`S/C` <- as.factor(dc$`S/C`)
+
+
+
+temp.dc <- dc %>% mutate(Pos = as.character(Pos))
+temp.dc$Pos[temp.dc$Pos == 'L' | temp.dc$Pos == 'R'] <- 'F'
+temp.dc$Pos <- as.factor(temp.dc$Pos)
+data <- temp.dc %>% dplyr::select(-c(1, 3))
+
+set.seed(1)
+k <- 10
+n <- dim(data)[1]
+p <- dim(data)[2]
+fold <- sample(k, n, replace = TRUE)
+g <- length(unique(data$Pos))
+prior <- tapply(data$Pos, data$Pos, function(x) length(x) / n) %>%
+  as.numeric
+
+vars <- names(data)[-1]
+# set <- subsets(length(vars), 2, vars)
+# rates <- c()
+# 
+# for (j in 1:(dim(set)[1])) {
+#   confuse <- matrix(rep(0, g^2), nrow = g)
+#   temp <- data %>% dplyr::select(c('Pos', set[j, ]))
+#   for (i in 1:k) {
+#     zcv <- lda(formula = Pos~., data = temp[fold != i,],
+#                prior = prior)
+#     ppcv <- predict(zcv, temp[fold == i, 2:3])
+#     confuse <- confuse + table(temp[fold == i, 'Pos'], ppcv$class)
+#   }
+#   rate <- 1 - sum(diag(confuse)) / sum(confuse)
+#   rates <- c(rates, rate)
+# }
+
+m <- 91
+all_rates <- c(1, 1)
+cv_vars <- vector(mode = 'list', length = m)
+cv_vars[[1]] <- NULL; cv_vars[[2]] <- NULL
+
+
+start_vars <- c('EV FOL', 'BkS/60')
+
+for (j in 3:m) {
+  next_vars <- vars[!(vars %in% start_vars)]
+  rates <- c()
+  for (var in next_vars) {
+    v <- c(start_vars, var)
+    confuse <- matrix(rep(0, g^2), nrow = g)
+    temp <- data %>% dplyr::select(c('Pos', v))
+    for (i in 1:k) {
+      zcv <- lda(formula = Pos~., data = temp[fold != i,],
+                 prior = prior)
+      ppcv <- predict(zcv, temp[fold == i, 2:(dim(temp)[2])])
+      confuse <- confuse + table(temp[fold == i, 'Pos'], ppcv$class)
+    }
+    rate <- 1 - sum(diag(confuse)) / sum(confuse)
+    rates <- c(rates, rate)
+  }
+  
+  all_rates <- c(all_rates, min(rates))
+  start_vars <- c(start_vars, next_vars[which.min(rates)])
+  cv_vars[[j]] <- start_vars
+}
+
+print(which.min(all_rates))
+print(min(all_rates))
+print(cv_vars[[which.min(all_rates)]])
+
+out_vars <- data.frame(vars = cv_vars[[which.min(all_rates)]])
+out_data <- data.frame(rates = all_rates)
+
+write.csv(out_vars, file = '../data/class-subset-vars.csv')
+write.csv(out_data, file = '../data/class-subset-data.csv')
--- src/class.R
+++ src/class.R
@@ -3,10 +3,10 @@ rm( list = ls() )
 # Make sure you're in the 'src' directory.
 # CLASSIFICATION ANALYSIS
 
-library(dplyr)
 library(ggplot2)
 library(lubridate)
 library(MASS)
+library(dplyr)
 
 # Load `df` and `ds`.
 load('../data/data.Rdata')
@@ -33,39 +33,38 @@ del <- c(del, '+/-', 'GP', 'GWG', 'OTG', '1g', 'SH iSAT', 'SH iSAT/60',
 
 dc <- df %>%
   .[, !(names(df) %in% dep)] %>%
-  dplyr::select(-c(1:2, 5:7, 9:11)) %>%
+  select(-c(1:2, 5:7, 9:11)) %>%
   as.data.frame
 dc$Pos <- as.factor(dc$Pos)
 dc$Ntnlty <- as.factor(dc$Ntnlty)
 dc$`S/C` <- as.factor(dc$`S/C`)
 
 
-my.da <- function(data, var, type = 'lda', prior = NULL) {
+my.da <- function(data, var, type = 'lda', priors = NULL) {
   set.seed(1)
   k <- 10
-  n <- dim(dc)[1]
-  p <- dim(dc)[2]
-  start <- 4
+  n <- dim(data)[1]
+  p <- dim(data)[2]
   fold <- sample(k, n, replace = TRUE)
   
   g <- length(unique(data[[var]]))
   confuse <- matrix(rep(0, g^2), nrow = g)
-  if (is.null(prior)) {
-    prior <- tapply(data[[var]], data[[var]], function(x) length(x) / n) %>%
+  if (is.null(priors)) {
+    priors <- tapply(data[[var]], data[[var]], function(x) length(x) / n) %>%
              as.numeric
   }
   
   for (i in 1:k) {
     if (type == 'lda') {
-      zcv <- lda(data[fold != i, var]~., data[fold != i, start:p],
-                 prior = prior)
+      zcv <- lda(data[fold != i, var]~., data[fold != i, 2:p],
+                 prior = priors)
     } else if (type == 'qda') {
-      zcv <- qda(data[fold != i, var]~., data[fold != i, start:p],
-                 prior = prior)
+      zcv <- qda(data[fold != i, var]~., data[fold != i, 2:p],
+                 prior = priors)
     } else {
       stop('Invalid type.')
     }
-    ppcv <- predict(zcv, data[fold == i, start:p])
+    ppcv <- predict(zcv, data[fold == i, 2:p])
     confuse <- confuse + table(data[fold == i, var], ppcv$class)
   }
   rate <- 1 - sum(diag(confuse)) / sum(confuse)
@@ -76,29 +75,25 @@ my.da <- function(data, var, type = 'lda', prior = NULL) {
 
 
 ## Position
-pos.lda <- my.da(dc, 'Pos', type = 'lda')
-pos.qda <- my.da(dc, 'Pos', type = 'qda')
+pos.lda <- dc %>% select(-c('S/C', 'Ntnlty')) %>% my.da('Pos', 'lda')
+pos.qda <- dc %>% select(-c('S/C', 'Ntnlty')) %>% my.da('Pos', 'qda')
 
 temp.dc <- dc %>% mutate(Pos = as.character(Pos))
 temp.dc$Pos[temp.dc$Pos == 'L' | temp.dc$Pos == 'R'] <- 'F'
 temp.dc$Pos <- as.factor(temp.dc$Pos)
 
-pos.lda.adj <- my.da(temp.dc, 'Pos', type = 'lda')
-pos.qda.adj <- my.da(temp.dc, 'Pos', type = 'qda')
+pos.lda.adj <- temp.dc %>% select(-c('S/C', 'Ntnlty')) %>% my.da('Pos', 'lda')
+pos.qda.adj <- temp.dc %>% select(-c('S/C', 'Ntnlty')) %>% my.da('Pos', 'qda')
+
+pos.vars <- read.csv('../data/class-subset-vars.csv', header = TRUE,
+                     stringsAsFactors = FALSE)$vars
+pos.lda.sub <- temp.dc %>% select(c('Pos', pos.vars)) %>% my.da('Pos', 'lda')
 
 
 ## Nationality
-nty.lda <- my.da(dc, 'Ntnlty', type = 'lda')
+nty.lda <- dc %>% select(-c('Pos', 'S/C')) %>% my.da('Ntnlty', 'lda')
 
 
 ## Shoots/Catches
-sc.lda <- my.da(dc, 'S/C', type = 'lda')
-sc.qda <- my.da(dc, 'S/C', type = 'qda')
-
-
-
-
-
-
-
-
+sc.lda <- dc %>% select(-c('Pos', 'Ntnlty')) %>% my.da('S/C', 'lda')
+sc.qda <- dc %>% select(-c('Pos', 'Ntnlty')) %>% my.da('S/C', 'qda')
