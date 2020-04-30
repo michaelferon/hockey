5652696bf8b75d3b2c6a2c4262a40e0527253514
Justin Nichols <niko@Justins-MacBook-Pro-2.local>
2020-04-30 03:11:29 -0600

    Updated regression and shrinkage to predict salary

--- src/reg.R
+++ src/reg.R
@@ -8,34 +8,26 @@ library(lubridate)
 library(leaps)
 
 # Load `df` and `ds`.
-load('../data/data.Rdata')
+load('../data/newData.Rdata')
 # df -- this is the complete dataset, unstandardized.
 # ds -- same as df, but standardized.
 
 
-# X is all the quantitative data.
-X <- df %>% select( -c(1:11) ) 
-dep <- c('P', 'SHG', 'SHP', 'FO', 'EV FO', 'PP FO', 'SH FO', 'SH FOW', 'SH FOL',
-         'OZ FO', 'NZ FO', 'DZ FO', 'On-Ice EV GD', 'ENP', 'Net Pen',
-         'Net Pen/60', 'G Msct', 'SHA', 'SHA2', 'PPA', 'PPA2', 'EVG', 'PPG', 'OTG',
-         'GWG', 'On-Ice PP GF', 'On-Ice SH GF', 'On-Ice EV GF', 'On-Ice EV GF%',
-         'ENG', '1g', 'SHG/60', 'PPG/60', 'PP GF/60', 'P/GP', 'EVP', 'PPP', 'S',
-         'S%', 'MsS', 'MsS Wide', 'MsS Over', 'MsS Post', 'MsS Cross', 'SHP/60',
-         'PP Shots', 'PP S%', 'PP S/60')
+# X is all the quantitative data, due to causing linear dependencies
+X.ind <- data %>% select( -c(1, 3:5) )
 
-X.ind <- X %>% select(-all_of(dep))
-num.features <- length(names(X.ind))-1
+num.features <- length( names(X.ind) ) - 1
 
 # forward selection performed works for 90 even with dependencies *doublecheck later*
-step.forward <- regsubsets( G~., data=X.ind, method="forward", nvmax = num.features)
+step.forward <- regsubsets( Salary ~ ., data = X.ind, method = "forward", nvmax = num.features )
 step.forward.sum <- summary(step.forward)
 
 # formulas for determining best model with many variables
 adjusted.fits <- as.data.frame( cbind( Rsqr = step.forward.sum$rsq, adjRsqr = step.forward.sum$adjr2, 
                                       bic = step.forward.sum$bic, cp = step.forward.sum$cp ) )
 
-# both r squared values are useless in this case due to amount of variables get rid
-best.models <- apply( adjusted.fits[3:4], 2, which.min )
+# r squared value useless in this case due to amount of variables get rid
+best.models <- apply( adjusted.fits[2:4], 2, which.min )
 
 # best model determined by bic, which usually produces a small model (penalty on more variables)
 best.bic <- as.data.frame( coef( step.forward.sum$obj, best.models[1] ) )
@@ -47,37 +39,37 @@ names(best.cp)[1] <- "value"
 
 set.seed(1)
 k = 10
-folds = sample(1:k, nrow(X.ind), replace=TRUE)
-cv.errors = matrix(NA, k, num.features, dimnames=list(NULL,c(1:num.features)))
+folds = sample( 1:k, nrow(X.ind), replace=TRUE )
+cv.errors = matrix( NA, k, num.features, dimnames = list( NULL,c(1:num.features) ) )
 
 for (j in 1:k){
-  best.fit = regsubsets(G ~., data=X.ind[folds!=j,], nvmax=num.features,method="forward")
-  testmat = model.matrix(G ~., data = X.ind[folds==j,])
+  best.fit = regsubsets( Salary ~., data=X.ind[folds!=j,], nvmax=num.features,method="forward" )
+  testmat = model.matrix( Salary ~., data = X.ind[folds==j,] )
   for (i in 1:num.features){
-    coefi = coef(best.fit, id=i)
-    xvars = names(coefi)
+    coefi = coef( best.fit, id=i )
+    xvars = names( coefi )
     pred = testmat[,xvars]%*%coefi
-    cv.errors[j,i] = mean((X.ind$G[folds==j]-pred)^2)
+    cv.errors[j,i] = mean( (X.ind$Salary[folds==j]-pred)^2 )
   }
 }
-msep <- apply(cv.errors,2,mean)
+msep <- apply( cv.errors, 2, mean )
 number.variables <- which.min(msep)
 msep.min <- min(msep)
 
-best.fit <- regsubsets(G ~., data=X.ind, nvmax=number.variables,method="forward")
-final.variables <- coef(best.fit,id=number.variables)
+best.fit <- regsubsets( Salary ~., data = X.ind, nvmax = number.variables, method = "forward" )
+final.variables <- coef( best.fit,id=number.variables )
 
 xvars = names(final.variables)
-testmat <- model.matrix(G ~., data=X.ind)
+testmat <- model.matrix( Salary ~., data=X.ind )
 Xmat <- testmat[,xvars]
 
-G.hat <- Xmat%*%final.variables
-msep2 <- mean((G.hat-X.ind$G)^2)
+Salary.hat <- Xmat %*% final.variables
+msep2 <- mean( (Salary.hat-X.ind$Salary)^2 )
 
-resid <- X.ind$G - G.hat
+resid <- X.ind$Salary - Salary.hat
 
-plot(G.hat, resid, pch=20, cex=0.5, axes = TRUE, xlab = "Fitted Values", ylab="Residuals")
-abline(h=0,col='red',lwd=2)
+plot( Salary.hat, resid, pch = 20, cex = 0.5, axes = TRUE, xlab = "Fitted Values", ylab = "Residuals" )
+abline( h = 0, col = 'red', lwd = 2 )
 
 
 
--- src/shrink.R
+++ src/shrink.R
@@ -9,22 +9,14 @@ library(leaps)
 library(glmnet)
 
 # Load `df` and `ds`.
-load('../data/data.Rdata')
+load('../data/newData.Rdata')
 # df -- this is the complete dataset, unstandardized.
 # ds -- same as df, but standardized.
 
 
 # X is all the quantitative data.
-X <- df %>% select( -c(1:11) ) 
-dep <- c('P', 'SHG', 'SHP', 'FO', 'EV FO', 'PP FO', 'SH FO', 'SH FOW', 'SH FOL',
-         'OZ FO', 'NZ FO', 'DZ FO', 'On-Ice EV GD', 'ENP', 'Net Pen',
-         'Net Pen/60', 'G Msct', 'SHA', 'SHA2', 'PPA', 'PPA2', 'EVG', 'PPG', 'OTG',
-         'GWG', 'On-Ice PP GF', 'On-Ice SH GF', 'On-Ice EV GF', 'On-Ice EV GF%',
-         'ENG', '1g', 'SHG/60', 'PPG/60', 'PP GF/60', 'P/GP', 'EVP', 'PPP', 'S',
-         'S%', 'MsS', 'MsS Wide', 'MsS Over', 'MsS Post', 'MsS Cross', 'SHP/60',
-         'PP Shots', 'PP S%', 'PP S/60')
+X.ind <- data %>% select( -c(1, 3:5) )
 
-X.ind <- X %>% select( -all_of(dep) )
 num.features <- length( names(X.ind) ) - 1
 
 set.seed(1)
@@ -33,15 +25,15 @@ grid = 10^seq(-2, 8, length=100)
 folds = sample(1:k, nrow(X.ind), replace=TRUE)
 cv.errors = matrix(NA, k, 100, dimnames=list(NULL, c(1:100)))
 
-x = model.matrix(G~., data=X.ind)[,-1]
-y = X.ind$G
+x = model.matrix(Salary ~ ., data=X.ind)[,-1]
+y = X.ind$Salary
 for (j in 1:k) {
   lasso = glmnet(x[folds!=j,], y[folds!=j], alpha=1, lambda=grid)
-  testmat = model.matrix(G~., data=X.ind[folds==j,])
+  testmat = model.matrix(G ~ ., data=X.ind[folds==j,])
   for (i in 1:100) {
     coefi = coef(lasso)[,i]
     pred = testmat%*%coefi
-    cv.errors[j, i] = mean((X.ind$G[folds==j]-pred)^2)
+    cv.errors[j, i] = mean((X.ind$Salary[folds==j]-pred)^2)
   }
 }
 
@@ -52,20 +44,20 @@ msep.min <- min(msep)
 
 lasso.full <- glmnet(x, y, alpha=1, lambda=grid)
 lasso.coef <- predict(lasso.full, s=lambda.min, type="coefficients")
-testmat <- model.matrix(G~., data=X.ind)
-G.hat <- testmat%*%lasso.coef
-msep.lasso <- mean((G.hat-X.ind$G)^2)
+testmat <- model.matrix(Salary ~ ., data=X.ind)
+Salary.hat <- testmat %*% lasso.coef
+msep.lasso <- mean((Salary.hat-X.ind$Salary)^2)
 
 # extract the sparse matrix elinating the . entries
 lass.coef.tibble<- tibble(name = lasso.coef@Dimnames[[1]][lasso.coef@i + 1], coefficient = lasso.coef@x)
 lass.coef.tibble$name<- lass.coef.tibble$name %>% str_replace_all("`", "")
 
 
-X.pred.lasso <- cbind(X.ind$G, X.ind %>% select(lass.coef.tibble$name[-1]))
-names(X.pred.lasso)[1] <- 'G'
+X.pred.lasso <- cbind(X.ind$Salary, X.ind %>% select(lass.coef.tibble$name[-1]))
+names(X.pred.lasso)[1] <- 'Salary'
 
-fit <- lm(G~., data=X.pred.lasso)
-plot(fit,cex=0.5,which=1)
+fit <- lm(Salary ~ ., data=X.pred.lasso)
+plot(fit,pch=19,cex=0.5,which=1)
 
 
 
