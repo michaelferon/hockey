7a426b7dd3b62da28d3960f97fd14e561d8fa586
Justin Nichols <jnichols2@mymail.mines.edu>
2020-05-03 04:23:43 -0600

    Plots added

--- src/reg.R
+++ src/reg.R
@@ -17,11 +17,20 @@ X.ind <- as_tibble(data %>% select( -c(1, 3:5) ) %>% scale())
 # start by fitting a simple linear model
 simple.model <- lm(Salary ~ ., data = X.ind)
 
-pdf(file="../plots/resid_simp_linear.pdf", bg="transparent", height=5)
+# plot of residuals vs. fitted (simple linear model)
+pdf(file="../plots/regression/resid_simp_linear.pdf", bg="transparent", width=6, height=4.8)
 plot(simple.model$fitted.values, simple.model$residuals, pch=20, cex=0.75,
      xlab=TeX("Fitted Values"), ylab = TeX("Residuals"))
-text(2.5,1.5, TeX(sprintf("$R^2 = %.3f", summary(simple.model)$r.squared)), cex=0.85, col='red')
-abline( h = 0, col = 'black', lwd = 1 )
+text(2.5,1.6, TeX(sprintf("Adj. $R^2 = %.3f", summary(simple.model)$adj.r.squared)), cex=0.85)
+abline( h = 0, col = 'red', lwd = 1, lty='dashed' )
+dev.off()
+
+# plot of actual vs. fitted
+pdf(file="../plots/regression/actVSfit_simp_linear.pdf", bg="transparent", width=6, height=4.8)
+plot(simple.model$fitted.values, X.ind$Salary, pch=20, cex=0.75,
+     xlab=TeX("Fitted Values"), ylab = TeX("Actual Values"))
+text(2.5,-0.5, TeX(sprintf("Adj. $R^2 = %.3f", summary(simple.model)$adj.r.squared)), cex=0.85)
+abline( a=0,b=1, col = 'red', lwd = 1, lty='dashed' )
 dev.off()
 
 num.features <- length( names(X.ind) ) - 1
@@ -46,6 +55,29 @@ row.names(best.bic) <- str_replace_all(row.names(best.bic),"`", "")
 best.cp <- as.data.frame( coef( step.forward.sum$obj, best.models$cp ) )
 names(best.cp)[1] <- "value"
 row.names(best.cp) <- str_replace_all(row.names(best.cp),"`", "")
+
+# gather data based only on the variables bic chose
+X.bic <- X.ind %>% select(row.names(best.bic)[-1])
+
+fitted.bic <- lm(X.ind$Salary ~ ., data=X.bic)$fitted.values
+
+# plot of residuals vs. fitted (simple linear model)
+pdf(file="../plots/regression/resid_forwd_bic.pdf", bg="transparent", width=6, height=4.8)
+plot(simple.model$fitted.values, simple.model$residuals, pch=20, cex=0.75,
+     xlab=TeX("Fitted Values"), ylab = TeX("Residuals"))
+text(2.5,1.6, TeX(sprintf("Adj. $R^2 = %.3f", step.forward.sum$adjr2[best.models$bic])), cex=0.85)
+abline( h = 0, col = 'red', lwd = 1, lty='dashed' )
+dev.off()
+
+# plot of actual vs. fitted
+pdf(file="../plots/regression/actVSfit_forwd_bic.pdf", bg="transparent", width=4.8, height=4.8)
+plot(fitted.bic, X.ind$Salary, pch=20, cex=0.75,
+     xlab=TeX("Fitted Values"), ylab = TeX("Actual Values"))
+text(2,-0.5, TeX(sprintf("Adj. $R^2 = %.3f", step.forward.sum$adjr2[best.models$bic])), cex=0.85)
+abline( a=0,b=1, col = 'red', lwd = 1, lty='dashed' )
+dev.off()
+
+
 
 # k fold cross validation with forward selection
 set.seed(1)
@@ -80,7 +112,7 @@ msep2 <- mean( (Salary.hat-X.ind$Salary)^2 )
 resid <- X.ind$Salary - Salary.hat
 
 plot( Salary.hat, resid, pch = 20, cex = 0.5, axes = TRUE, xlab = "Fitted Values", ylab = "Residuals" )
-abline( h = 0, col = 'red', lwd = 2 )
+abline( h = 0, col = 'black', lwd = 1 )
 
 
 
